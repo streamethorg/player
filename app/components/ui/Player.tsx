abb7c14fa5242c8e6cab7a1cfff1c7ee163f243e
greatsamist <ifegbenga@gmail.com>
2024-05-10 10:02:37 +0100

    update player

--- app/components/ui/Player.tsx
+++ app/components/ui/Player.tsx
@@ -13,7 +13,7 @@ import {
   UnmuteIcon,
 } from "@livepeer/react/assets";
 import * as Player from "@livepeer/react/player";
-// import * as Popover from "@radix-ui/react-popover";
+import * as Popover from "@radix-ui/react-popover";
 
 import React from "react";
 
@@ -129,7 +129,7 @@ export function PlayerWithControls(props: { src: Src[] | null }) {
             </div>
             <div className="flex sm:flex-1 md:flex-[1.5] justify-end items-center gap-2.5">
               <Player.FullscreenIndicator matcher={false} asChild>
-                {/* <Settings className="text-white w-6 h-6 transition flex-shrink-0" /> */}
+                <Settings className="text-white w-6 h-6 transition flex-shrink-0" />
               </Player.FullscreenIndicator>
               {/* <Clip className="flex items-center w-6 h-6 justify-center" /> */}
 
@@ -242,131 +242,131 @@ export const PlayerLoading = ({
 //   )
 // }
 
-// export const Settings = React.forwardRef(function Search(
-//   { className }: { className?: string },
-//   ref: React.Ref<HTMLButtonElement> | undefined
-// ) {
-//   return (
-//     <Popover.Root>
-//       <Popover.Trigger ref={ref} asChild>
-//         <button
-//           type="button"
-//           className={className}
-//           aria-label="Playback settings"
-//           onClick={(e) => {
-//             console.log(e);
-//             e.stopPropagation();
-//           }}
-//         >
-//           <SettingsIcon />
-//         </button>
-//       </Popover.Trigger>
-//       <Popover.Portal>
-//         <Popover.Content
-//           className=" w-60 rounded-md text-white bg-black/50 border border-white/50 backdrop-blur-md p-3 shadow-md outline-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2"
-//           side="top"
-//           alignOffset={-70}
-//           align="end"
-//           onClick={(e) => e.stopPropagation()}
-//         >
-//           <div className="flex flex-col gap-2">
-//             <p className="text-white/90 font-medium text-sm mb-1">Settings</p>
-//             <Player.LiveIndicator
-//               matcher={false}
-//               className="gap-2 flex-col flex"
-//             >
-//               <label
-//                 className="text-xs text-white/90 font-medium"
-//                 htmlFor="speedSelect"
-//               >
-//                 Playback speed
-//               </label>
-//               <Player.RateSelect name="speedSelect">
-//                 <Player.SelectTrigger
-//                   className="inline-flex items-center justify-between rounded-sm px-1 outline-1 outline-white/50 text-xs leading-none h-7 gap-1 outline-none"
-//                   aria-label="Playback speed"
-//                 >
-//                   <Player.SelectValue placeholder="Select a speed..." />
-//                   <Player.SelectIcon>
-//                     {/* <ChevronDownIcon className="h-4 w-4" /> */}
-//                   </Player.SelectIcon>
-//                 </Player.SelectTrigger>
-//                 <Player.SelectPortal>
-//                   <Player.SelectContent className=" overflow-hidden bg-white border border-white/50 rounded-sm">
-//                     <Player.SelectViewport className="p-1">
-//                       <Player.SelectGroup>
-//                         <RateSelectItem value={0.5}>0.5x</RateSelectItem>
-//                         <RateSelectItem value={0.75}>0.75x</RateSelectItem>
-//                         <RateSelectItem value={1}>1x (normal)</RateSelectItem>
-//                         <RateSelectItem value={1.25}>1.25x</RateSelectItem>
-//                         <RateSelectItem value={1.5}>1.5x</RateSelectItem>
-//                         <RateSelectItem value={1.75}>1.75x</RateSelectItem>
-//                         <RateSelectItem value={2}>2x</RateSelectItem>
-//                       </Player.SelectGroup>
-//                     </Player.SelectViewport>
-//                   </Player.SelectContent>
-//                 </Player.SelectPortal>
-//               </Player.RateSelect>
-//             </Player.LiveIndicator>
-//             <div className="gap-2 flex-col flex">
-//               <label
-//                 className="text-xs text-white/90 font-medium"
-//                 htmlFor="qualitySelect"
-//               >
-//                 Quality
-//               </label>
-//               <Player.VideoQualitySelect
-//                 name="qualitySelect"
-//                 defaultValue="1.0"
-//               >
-//                 <Player.SelectTrigger
-//                   className="inline-flex items-center justify-between rounded-sm px-1 outline-1 outline-white/50 text-xs leading-none h-7 gap-1 outline-none"
-//                   aria-label="Playback quality"
-//                 >
-//                   <Player.SelectValue placeholder="Select a quality..." />
-//                   <Player.SelectIcon>
-//                     {/* <ChevronDownIcon className="h-4 w-4" /> */}
-//                   </Player.SelectIcon>
-//                 </Player.SelectTrigger>
-//                 <Player.SelectPortal>
-//                   <Player.SelectContent className="overflow-hidden bg-white rounded-sm">
-//                     <Player.SelectViewport className="p-[5px]">
-//                       <Player.SelectGroup>
-//                         <VideoQualitySelectItem value="auto">
-//                           Auto (HD+)
-//                         </VideoQualitySelectItem>
-//                         <VideoQualitySelectItem value="1080p">
-//                           1080p (HD)
-//                         </VideoQualitySelectItem>
-//                         <VideoQualitySelectItem value="720p">
-//                           720p
-//                         </VideoQualitySelectItem>
-//                         <VideoQualitySelectItem value="480p">
-//                           480p
-//                         </VideoQualitySelectItem>
-//                         <VideoQualitySelectItem value="360p">
-//                           360p
-//                         </VideoQualitySelectItem>
-//                       </Player.SelectGroup>
-//                     </Player.SelectViewport>
-//                   </Player.SelectContent>
-//                 </Player.SelectPortal>
-//               </Player.VideoQualitySelect>
-//             </div>
-//           </div>
-//           <Popover.Close
-//             className="rounded-full h-5 w-5 inline-flex items-center justify-center absolute top-2.5 right-2.5 outline-none"
-//             aria-label="Close"
-//           >
-//             <p>close</p>
-//             {/* <XIcon /> */}
-//           </Popover.Close>
-//           <Popover.Arrow className="fill-white/50" />
-//         </Popover.Content>
-//       </Popover.Portal>
-//     </Popover.Root>
-//   );
-// });
+export const Settings = React.forwardRef(function Search(
+  { className }: { className?: string },
+  ref: React.Ref<HTMLButtonElement> | undefined
+) {
+  return (
+    <Popover.Root>
+      <Popover.Trigger ref={ref} asChild>
+        <button
+          type="button"
+          className={className}
+          aria-label="Playback settings"
+          onClick={(e) => {
+            console.log(e);
+            e.stopPropagation();
+          }}
+        >
+          <SettingsIcon />
+        </button>
+      </Popover.Trigger>
+      <Popover.Portal>
+        <Popover.Content
+          className=" w-60 rounded-md text-white bg-black/50 border border-white/50 backdrop-blur-md p-3 shadow-md outline-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2"
+          side="top"
+          alignOffset={-70}
+          align="end"
+          onClick={(e) => e.stopPropagation()}
+        >
+          <div className="flex flex-col gap-2">
+            <p className="text-white/90 font-medium text-sm mb-1">Settings</p>
+            <Player.LiveIndicator
+              matcher={false}
+              className="gap-2 flex-col flex"
+            >
+              <label
+                className="text-xs text-white/90 font-medium"
+                htmlFor="speedSelect"
+              >
+                Playback speed
+              </label>
+              <Player.RateSelect name="speedSelect">
+                <Player.SelectTrigger
+                  className="inline-flex items-center justify-between rounded-sm px-1 outline-1 outline-white/50 text-xs leading-none h-7 gap-1 outline-none"
+                  aria-label="Playback speed"
+                >
+                  <Player.SelectValue placeholder="Select a speed..." />
+                  <Player.SelectIcon>
+                    {/* <ChevronDownIcon className="h-4 w-4" /> */}
+                  </Player.SelectIcon>
+                </Player.SelectTrigger>
+                <Player.SelectPortal>
+                  <Player.SelectContent className=" overflow-hidden bg-white border border-white/50 rounded-sm">
+                    <Player.SelectViewport className="p-1">
+                      <Player.SelectGroup>
+                        <RateSelectItem value={0.5}>0.5x</RateSelectItem>
+                        <RateSelectItem value={0.75}>0.75x</RateSelectItem>
+                        <RateSelectItem value={1}>1x (normal)</RateSelectItem>
+                        <RateSelectItem value={1.25}>1.25x</RateSelectItem>
+                        <RateSelectItem value={1.5}>1.5x</RateSelectItem>
+                        <RateSelectItem value={1.75}>1.75x</RateSelectItem>
+                        <RateSelectItem value={2}>2x</RateSelectItem>
+                      </Player.SelectGroup>
+                    </Player.SelectViewport>
+                  </Player.SelectContent>
+                </Player.SelectPortal>
+              </Player.RateSelect>
+            </Player.LiveIndicator>
+            <div className="gap-2 flex-col flex">
+              <label
+                className="text-xs text-white/90 font-medium"
+                htmlFor="qualitySelect"
+              >
+                Quality
+              </label>
+              <Player.VideoQualitySelect
+                name="qualitySelect"
+                defaultValue="1.0"
+              >
+                <Player.SelectTrigger
+                  className="inline-flex items-center justify-between rounded-sm px-1 outline-1 outline-white/50 text-xs leading-none h-7 gap-1 outline-none"
+                  aria-label="Playback quality"
+                >
+                  <Player.SelectValue placeholder="Select a quality..." />
+                  <Player.SelectIcon>
+                    {/* <ChevronDownIcon className="h-4 w-4" /> */}
+                  </Player.SelectIcon>
+                </Player.SelectTrigger>
+                <Player.SelectPortal>
+                  <Player.SelectContent className="overflow-hidden bg-white rounded-sm">
+                    <Player.SelectViewport className="p-[5px]">
+                      <Player.SelectGroup>
+                        <VideoQualitySelectItem value="auto">
+                          Auto (HD+)
+                        </VideoQualitySelectItem>
+                        <VideoQualitySelectItem value="1080p">
+                          1080p (HD)
+                        </VideoQualitySelectItem>
+                        <VideoQualitySelectItem value="720p">
+                          720p
+                        </VideoQualitySelectItem>
+                        <VideoQualitySelectItem value="480p">
+                          480p
+                        </VideoQualitySelectItem>
+                        <VideoQualitySelectItem value="360p">
+                          360p
+                        </VideoQualitySelectItem>
+                      </Player.SelectGroup>
+                    </Player.SelectViewport>
+                  </Player.SelectContent>
+                </Player.SelectPortal>
+              </Player.VideoQualitySelect>
+            </div>
+          </div>
+          <Popover.Close
+            className="rounded-full h-5 w-5 inline-flex items-center justify-center absolute top-2.5 right-2.5 outline-none"
+            aria-label="Close"
+          >
+            <p>close</p>
+            {/* <XIcon /> */}
+          </Popover.Close>
+          <Popover.Arrow className="fill-white/50" />
+        </Popover.Content>
+      </Popover.Portal>
+    </Popover.Root>
+  );
+});
 
 export const RateSelectItem = React.forwardRef<
   HTMLDivElement,
